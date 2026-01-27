2f2bf92608ed5562c196b437891ef889c9c81ae1
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2026-01-27 14:31:31 -0600

    [scripts] picker: done!
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- maintenance/scripts/pick_project.sh
+++ maintenance/scripts/pick_project.sh
@@ -1,22 +1,56 @@
 #!/usr/bin/env bash
 set -e
-DATA_VOL="${DATA_VOL:-/data}"
-TODO_VOL="${TODO_VOL:-/todo}"
-get_random_id() {
-  test -f "${DATA_VOL}/.cncf_next_project_id" || return 1
-  cat "${DATA_VOL}/.cncf_next_project_id"
+
+source "../include/helpers/config.sh"
+source "../include/helpers/data.sh"
+source "../include/helpers/errors.sh"
+
+wip_project_present() {
+  test -n "$(_get_file_from_todo_vol 'wip_project.txt')"
 }
 
-fail_if_not_running_in_container() {
-  # shellcheck disable=SC2154
-  { test -f /.dockerenv || test "$container" == 'podman'; } && return 0
-
-  >&2 echo "ERROR: This script is meant to be run within a container."
-  exit 1
+picked_project() {
+  cat "$(_get_file_from_todo_vol 'wip_project.txt')"
 }
 
-fail_if_not_running_in_container
-if random_id_already_generated || last_project_is_now_done
-then regenerate_random_id
+picked_project_already_finished() {
+  grep -Eq "$(picked_project)" < <(_get_file_from_todo_vol 'finished_projects.txt')
+}
+
+unpick_project() {
+  _recreate_file_in_todo_vol 'wip_project.txt'
+}
+
+pick_next_project() {
+  num_todo_projects=$(cat "$(_get_file_from_todo_vol 'todo_projects.txt')" | wc -l)
+  line="$((1 + RANDOM % num_todo_projects))"
+  sed -n "${line}p" "$(_get_file_from_todo_vol 'todo_projects.txt')"
+}
+
+save_picked_project() {
+  echo "$1" > "$(_get_file_from_todo_vol 'wip_project.txt')"
+}
+
+picked_project_valid() {
+  grep -Eq "^$(picked_project)$" "$(_get_file_from_todo_vol 'todo_projects.txt')"
+}
+
+_fail_if_not_running_in_container
+if wip_project_present
+then
+  if ! picked_project_valid
+  then
+    >&2 echo "ERROR: You're working on '$(picked_project)', but it's not valid?"
+    exit 1
+  fi
+  if ! picked_project_already_finished
+  then
+    >&2 echo "ERROR: You're still working on '$(picked_project)'; ship it if you're done!"
+    exit 1
+  fi
+  unpick_project
+else _create_file_in_todo_vol 'wip_project.txt'
 fi
->&2 echo "INFO: Project: $(project_from_random_id) [id: $(random_id)"
+project=$(pick_next_project)
+save_picked_project "$project"
+>&2 echo "INFO: Congrats! You're working on '$(picked_project)'. Get crackin'!"
