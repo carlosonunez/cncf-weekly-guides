b8d10f19277412035b1d438f07109aac5a74b11e
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2026-01-27 14:31:02 -0600

    [scripts] builder: remove partners, supporters and CSP services
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- maintenance/scripts/build_projects.sh
+++ maintenance/scripts/build_projects.sh
@@ -1,56 +1,41 @@
 #!/usr/bin/env bash
-set -ex
-DATA_VOL="${DATA_VOL:-/data}"
-TODO_VOL="${TODO_VOL:-/todo}"
-CONFIG_YAML_PATH="${CONFIG_YAML_PATH:-/config.yaml}"
-CNCF_LANDSCAPE_URL="https://raw.githubusercontent.com/cncf/landscape/refs/heads/master/landscape.yml"
+set -e
 
-_fail_if_empty() {
-  test -n "$1" && return 0
-
-  >&2 echo "ERROR: $2"
-  exit 1
-}
-
-blog_sitemap_url() {
-  url=$(yq -r '.urls.blog' "$CONFIG_YAML_PATH")
-  test -z "$url" && return 1
-
-  echo "${url}/sitemap.xml"
-}
+source "../include/helpers/config.sh"
+source "../include/helpers/data.sh"
+source "../include/helpers/errors.sh"
 
 get_current_cncf_projects_by_name() {
-  curl -L "$CNCF_LANDSCAPE_URL" |
+  curl -sSL "https://raw.githubusercontent.com/cncf/landscape/refs/heads/master/landscape.yml" |
     yq -r '.landscape |
 to_entries[] |
 .value |
 to_entries[] |
 select(.key == "subcategories") |
-.value[].items[] |
-select(.name | contains("(member)") | not) |
-.name' |
+.value[].items[].name' |
+    grep -Ev ' \(.*\)$' |
     sort -u
 }
 
 get_done_cncf_weekly_projects_from_blog() {
-  url=$(blog_sitemap_url)
-  curl -L "$url" | yq -p=xml \
+  blog_url=$(_get_from_config '.urls.blog') || return 1
+  curl -sSL "${blog_url}/sitemap.xml" | yq -p=xml \
     '.urlset.url[] |
       select( 
         (.loc | contains("-init") | not) and 
         (.loc | contains("cncf-weekly-")) 
       ) | 
-      ((.loc | split("-"))[-1] | sub("/",""))' |
+      .loc' |
+  sed -E 's/.*cncf-weekly-[0-9]{1,}-// ; s#/$##' |
   sort -u
 }
 
 get_last_list_of_cncf_projects_by_name() {
-  test -f "${DATA_VOL}/cncf_projects_last_list.txt" || return 0
-  cat "${DATA_VOL}/cncf_projects_last_list.txt"
+  cat "$(_get_file_from_data_vol 'cncf_projects_last_list.txt')"
 }
 
 update_last_cncf_projects_list() {
-  echo "$1" > "${DATA_VOL}/cncf_projects_last_list.txt"
+  echo "$1" > "$(_get_file_from_data_vol 'cncf_projects_last_list.txt')"
 }
 
 show_cncf_projects_diff() {
@@ -61,29 +46,24 @@ show_cncf_projects_diff() {
   echo "$delta"
 }
 
-fail_if_not_running_in_container() {
-  # shellcheck disable=SC2154
-  { test -f /.dockerenv || test "$container" == 'podman'; } && return 0
-
-  >&2 echo "ERROR: This script is meant to be run within a container."
-  exit 1
-}
-
 create_done_list() {
   local all finished
   all="$1"
   finished="$2"
-  comm -12 <(echo "$all") <(echo "$finished") > "${TODO_VOL}/finished_projects.txt"
+  _recreate_file_in_todo_vol 'finished_projects.txt'
+  echo "$all" | grep -Ei "^($(tr '\n' '|' <<< "$finished" | sed -E 's/\|$//'))" > "$(_get_file_from_todo_vol 'finished_projects.txt')"
 }
 
 create_todo_list() {
   local all finished
   all="$1"
   finished="$2"
-  comm -23 <(echo "$all") <(echo "$finished") > "${TODO_VOL}/todo_projects.txt"
+  _recreate_file_in_todo_vol 'todo_projects.txt'
+  echo "$all" | grep -Evi "^($(tr '\n' '|' <<< "$finished" | sed -E 's/\|$//'))" > "$(_get_file_from_todo_vol 'todo_projects.txt')"
 }
 
-fail_if_not_running_in_container
+_fail_if_not_running_in_container
+
 all_cncf_projects=$(get_current_cncf_projects_by_name)
 _fail_if_empty "$all_cncf_projects" "Couldn't get CNCF projects."
 last_cncf_projects=$(get_last_list_of_cncf_projects_by_name)
